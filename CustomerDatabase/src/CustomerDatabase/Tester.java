ead85858a75769b2842fa947e5d244792f5e4705
GBTC440012UR <GBTC440012UR@GBTC363I12W7.wdce.local>
2015-10-22 13:40:36 -0400

    second

--- CustomerDatabase/src/CustomerDatabase/Tester.java
+++ CustomerDatabase/src/CustomerDatabase/Tester.java
@@ -3,23 +3,25 @@ package CustomerDatabase;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 
 public class Tester {
 
   @Test
   public static void Test() {
+	  final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 	  CustomerApp tester=new CustomerApp();
-    // MyClass is tested
+    
 	  CustomerApp.printInfo("Ford");
-
+	  tester.printInfo("Ford");
     // assert statements
     assertSame("Customer number: 17"+
 "Dr. Kelly Shackelford"+
 "4332 Barrington Court"+
 "Camden AR 71701"+
 "KellyTShackelford@cuvox.de"+
-"Crushing grinding and polishing machine operator at Buena Vista Garden Maintenance", tester.printInfo("Ford"));
+"Crushing grinding and polishing machine operator at Buena Vista Garden Maintenance", systemOutRule.getLog());
     
   }
 
